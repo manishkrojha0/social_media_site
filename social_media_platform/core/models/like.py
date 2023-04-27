fa3c6923bb9f3bf06a22f29fc6cd0d3456c7c0bf
Manish Kumar Ojha <manishkrojha0@gmail.com>
2023-04-28 02:49:43 +0530

    added mig for like

--- social_media_platform/core/models/like.py
+++ social_media_platform/core/models/like.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Like(models.Model):
     """Model class for like."""
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name='likes')
-    comment = models.ForeignKey("Comment", on_delete=models.CASCADE, related_name='likes')
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE, related_name='likes', null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
